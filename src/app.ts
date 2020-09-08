9e0b8ca323e947e10804642cc114e0744596bc06
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-08 08:23:58 +0300

    function types and callbacks

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,12 @@ function printResult(num: number): void {
     console.log('Result:' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (a: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+
+}
+
 printResult(add(3, 4))
 
 let combineValues: (a: number, b: number) => number;
@@ -14,4 +20,6 @@ combineValues = add;
 // combineValues = printResult;
 
 console.log(combineValues(3, 9));
+
+addAndHandle(12, 18, (result) => { console.log(result) })
 
