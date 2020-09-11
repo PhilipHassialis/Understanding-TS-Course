9ee5353107b390d58cf7959d0f4b9078de48c3d8
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-09-11 08:32:04 +0300

    next gen ts js part 1

--- app.ts
+++ app.ts
@@ -12,4 +12,37 @@ function clickHandler(message: string) {
 //     console.log("Clicked!")
 // })
 
+const addArrow = (a: number, b: number) => a + b;
+
+const sqrArrow = (a: number) => a * a;
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+if (button) {
+    button.addEventListener('click', event => {
+        console.log(event);
+    });
+}
+
+const addArrowDefaultValue = (a: number, b: number = 1) => a + b;
+
+addArrowDefaultValue(13);
+
+
 button?.addEventListener('click', clickHandler.bind(null, "my data"))
+
+const hobbies = ['coding', 'wanking'];
+const activities = ['youtubing'];
+
+activities.push(...hobbies);
+
+const empUser = {
+    userName: 'chasialisp',
+    firstName: 'Philip Alexander',
+    lastName: 'Hassialis'
+};
+
+const copiedEmpUser = { ...empUser };
+
+
+
